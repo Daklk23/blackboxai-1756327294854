e324373c3cb3cc1af7f6b8845a1f01e9506d0105
Daklk23 <lukagxfz@gmail.com>
2025-08-27 22:42:01 +0200

    Add vantage-roleplay/components/logo.tsx 1756327321634

---
+++ vantage-roleplay/components/logo.tsx
@@ -0,0 +1,8 @@
+export function Logo() {
+  return (
+    <div className="w-8 h-8 bg-lime-500 rounded-lg flex items-center justify-center">
+      {/* TODO: Replace with actual logo image */}
+      <span className="text-black font-bold text-lg">V</span>
+    </div>
+  )
+}
